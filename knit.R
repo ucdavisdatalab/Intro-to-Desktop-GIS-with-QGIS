99effa8f3fab171f044af5ff3c8a0c939bc3fbc4
Michele Tobias <mmtobias@ucdavis.edu>
2024-04-08 15:25:13 -0700

    update to current template code

--- knit.R
+++ knit.R
@@ -1,5 +1,28 @@
 #!/usr/bin/env Rscript
 
-# Settings for bookdown are in `_bookdown.yml` and settings for pandoc are in
-# `index.Rmd`.
-bookdown::render_book("index.Rmd", output_dir="./docs")
+show_help = function() {
+  # ' Show a help message
+  cat("Render a workshop reader website with Bookdown.\nArguments:\n")
+  cat("[-h | --help]\t Show this help message\n")
+  cat("[-p | --pdf]\t Include to render a PDF while building site\n")
+  quit()
+}
+
+main = function(args) {
+  # ' Run the script
+  if ("-h" %in% args || "--help" %in% args) {
+    show_help()
+  }
+  if ("-p" %in% args || "--pdf" %in% args) {
+    bookdown::render_book(
+      "index.Rmd", output_format = "pdf_document", output_file = "docs/_main"
+    )
+  }
+  bookdown::render_book("index.Rmd")
+}
+
+
+if (!interactive()) {
+  args = commandArgs(trailingOnly=TRUE)
+  main(args)
+}
